5549143ee29d87b84450dcd1c2c9b080c10c5560
alaeddine beljebbar <alaeddine.beljebbar@brickblock.io>
2021-06-12 22:00:31 +0200

    Hello humans

---
+++ README.md
@@ -0,0 +1,1 @@
+# A tool created for humans
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <p> l3alawil lm9aaaaaaaawd</p>
+    </div>
+  )
+}
+
+export default App
